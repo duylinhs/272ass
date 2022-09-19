2a76d823a0490bfe639225809b036275c4c4cab5
User <User@DESKTOP-BSUC0K6>
2022-09-20 01:21:29 +1000

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_272ass.MvcApplication" Language="C#" %>
---
+++ Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _272ass.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter your User Name")]
+        [Display(Name = "User Name")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+        [NotMapped]
+        public List<SelectListItem> Roles { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "admin", Text = "Administrator" },
+            new SelectListItem { Value = "organiser", Text = "Event Organiser" },
+            new SelectListItem { Value = "attendee", Text = "User"  },
+        };
+        public Boolean Deleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastEdit { get; set; }
+    }
+}
